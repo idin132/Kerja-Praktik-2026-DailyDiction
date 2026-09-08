6f07e3607012ad140217bce36321ee2d0b9ab94a
Rizqiww <rizkiakbarfadilah2005@gmail.com>
2026-09-08 20:56:48 +0700

    fix: safe create_comments_table migration

--- DailyDiction-Backend/database/migrations/2026_08_24_231817_create_comments_table.php
+++ DailyDiction-Backend/database/migrations/2026_08_24_231817_create_comments_table.php
@@ -4,19 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('article_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->text('comment');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('comments')) {
+            Schema::create('comments', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('article_id');
+                $table->unsignedBigInteger('user_id');
+                $table->text('comment');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
