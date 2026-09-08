f810f8cec4c6a51741d103e3265d7f1a56233f1a
Rizqiww <rizkiakbarfadilah2005@gmail.com>
2026-09-08 20:59:41 +0700

    fix: safe add_likes_to_articles_table migration

--- DailyDiction-Backend/database/migrations/2026_08_24_231909_add_likes_to_articles_table.php
+++ DailyDiction-Backend/database/migrations/2026_08_24_231909_add_likes_to_articles_table.php
@@ -4,14 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->unsignedBigInteger('likes_count')->default(0)->after('is_published');
+            if (!Schema::hasColumn('articles', 'likes_count')) {
+                $table->bigInteger('likes_count')->unsigned()->default(0);
+            }
         });
     }
 
@@ -21,7 +24,9 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            if (Schema::hasColumn('articles', 'likes_count')) {
+                $table->dropColumn('likes_count');
+            }
         });
     }
 };
