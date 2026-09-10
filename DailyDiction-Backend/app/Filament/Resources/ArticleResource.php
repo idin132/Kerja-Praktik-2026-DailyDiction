043109ff92688d995e91d4aaa24c59dd987122d1
Rizqiww <rizkiakbarfadilah2005@gmail.com>
2026-09-10 15:30:00 +0700

    fix: remove Alpine click outside blur from Tiptap editor to prevent focus lock

--- DailyDiction-Backend/app/Filament/Resources/ArticleResource.php
+++ DailyDiction-Backend/app/Filament/Resources/ArticleResource.php
@@ -48,7 +48,7 @@ class ArticleResource extends Resource
                     ->label('Title')
                     ->required()
                     ->maxLength(255)
-                    ->lazy() // JANGAN GUNAKAN live(onBlur: true)
+                    ->lazy()
                     ->afterStateUpdated(function (string $operation, ?string $state, Set $set) {
                         if ($operation === 'create') {
                             $set('slug', Str::slug($state));
@@ -131,12 +131,11 @@ class ArticleResource extends Resource
                     ->dehydrated(false)
                     ->columnSpanFull(),
 
-                // UBAH IMAGE URL DARI ->live(onBlur: true) MENJADI ->lazy()
                 Forms\Components\TextInput::make('image_url')
                     ->label('Thumbnail Artikel (URL Gambar)')
                     ->url()
                     ->placeholder('https://example.com/image.jpg')
-                    ->lazy() // JANGAN GUNAKAN live(onBlur: true)
+                    ->lazy()
                     ->maxLength(2000)
                     ->visible(fn(Get $get) => $get('thumbnail_mode') !== 'file')
                     ->required(fn(Get $get) => $get('thumbnail_mode') !== 'file')
@@ -162,13 +161,11 @@ class ArticleResource extends Resource
                     ->required(fn(Get $get) => $get('thumbnail_mode') === 'file')
                     ->dehydrated(fn(Get $get) => $get('thumbnail_mode') === 'file'),
 
-
                 Forms\Components\Placeholder::make('image_preview')
                     ->label('Preview Thumbnail')
                     ->content(function (Get $get) {
                         $mode = $get('thumbnail_mode');
 
-                        // Mode file: preview sudah ditangani oleh FileUpload component di atas
                         if ($mode === 'file') {
                             return new HtmlString('<span class="text-xs text-gray-400">Preview tersedia di area upload di atas.</span>');
                         }
@@ -193,19 +190,13 @@ class ArticleResource extends Resource
                     ->required()
                     ->columnSpanFull(),
 
+                // TIPTAP EDITOR YANG SUDAH DIBERSIHKAN
                 TiptapEditor::make('content')
                     ->label('Konten Artikel')
                     ->extraAttributes([
-                        'wire:ignore.self' => true,
-                        'x-on:focusout.stop' => '',
+                        'wire:ignore' => true,
                         'class' => '[&_.ProseMirror]:!caret-color-white [&_.tiptap-editor-toolbar]:!static',
                         'style' => 'min-height: 450px;',
-                        // Tambahkan ini:
-                        'x-data' => '{}',
-                        'x-on:click.outside' => '$el.querySelector(".ProseMirror")?.blur()',
-                    ])
-                    ->extraInputAttributes([
-                        'tabindex' => '0',
                     ])
                     ->disableBubbleMenus()
                     ->disableFloatingMenus()
